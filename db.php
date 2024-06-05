a0318b888fa01bfd46badd2763cd709de2b559b5
FISU02 <119970288+FISU02@users.noreply.github.com>
2024-06-05 13:22:13 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-const HOST = 'localhost';
-const USERNAME = 'root';
-const PASSWORD = '';
-const DBNAME = 'todo';
+const HOST = 'felipeapps-server.mysql.database.azure.com';
+const USERNAME = 'enqtjhopuc';
+const PASSWORD = 'Filip123';
+const DBNAME = 'felipeapps-database';
 $dbcon = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
 
 if ($dbcon->connect_error) {
